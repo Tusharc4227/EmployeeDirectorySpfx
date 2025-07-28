2689d09ed9e542a97fcb1cf2a46f7c10f1b0c379
unknown <tisharc4227@gmail.com>
2025-07-28 17:16:57 +0530

    pnp setup initial

--- src/webparts/employeeDirectory/components/EmployeeDirectory.tsx
+++ src/webparts/employeeDirectory/components/EmployeeDirectory.tsx
@@ -1,43 +1,52 @@
 import * as React from 'react';
-import styles from './EmployeeDirectory.module.scss';
+// import styles from './EmployeeDirectory.module.scss';
 import type { IEmployeeDirectoryProps } from './IEmployeeDirectoryProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-
-export default class EmployeeDirectory extends React.Component<IEmployeeDirectoryProps> {
+// import { escape } from '@microsoft/sp-lodash-subset';
+import { getSP } from '../pnp';
+import { SPFI } from '@pnp/sp';
+interface IEmployeeItem{
+  Title:String,
+  Id:number
+}
+interface IEmployeeState{
+  employees:IEmployeeItem[]
+}
+export default class EmployeeDirectory extends React.Component<IEmployeeDirectoryProps,IEmployeeState> {
+  private sp : SPFI;
+  constructor(props:IEmployeeDirectoryProps){
+    super(props);
+    this.state={
+      employees:[]
+    }
+    this.sp=getSP(props.context);
+  }
+  public componentDidMount(): void {
+    this.getEmployeeDetails();
+  }
+  private getEmployeeDetails =  async (): Promise<void> => {
+    //const employees:IEmployeeItem[] = 
+    const items = await this.sp.web.lists.getByTitle('Employees').items.select("Id","Title","Email","Department","Designation","PhotoURL");
+    console.log(items);
+    //return employees
+  }
   public render(): React.ReactElement<IEmployeeDirectoryProps> {
+    
     const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
+      // description,
+      // isDarkTheme,
+      // environmentMessage,
+      // hasTeamsContext,
+      // userDisplayName,
+      context
     } = this.props;
-
+    console.log("this",context)
     return (
-      <section className={`${styles.employeeDirectory} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
+     <div>
+      <h2>Employee Details</h2>
+      <ul>
+        {}
+      </ul>
+     </div>
     );
   }
 }
