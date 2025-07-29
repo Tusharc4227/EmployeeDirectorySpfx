72634fb5bd1e2905dfb646411a1dc1d7bcd8865c
ruchivats363 <41415865+ruchivats363@users.noreply.github.com>
2025-07-29 12:33:13 +0530

    detaillist setup of fluentui

--- src/webparts/employeeDirectory/components/EmployeeDirectory.tsx
+++ src/webparts/employeeDirectory/components/EmployeeDirectory.tsx
@@ -4,14 +4,18 @@ import type { IEmployeeDirectoryProps } from './IEmployeeDirectoryProps';
 // import { escape } from '@microsoft/sp-lodash-subset';
 import { getSP } from '../pnp';
 import { SPFI } from '@pnp/sp';
-import { DetailsList,IColumn } from '@fluentui/react';
+import { DetailsList,IColumn, Image, ImageFit } from '@fluentui/react';
 interface IEmployeeItem{
   Title:String,
   Id:number,
   Email:String,
   Department:String,
   Designation:String,
- // PhotoURL:Object
+  PhotoURL:Photo
+} 
+interface Photo{
+  Description:String,
+  Url:String
 }
 interface IEmployeeState{
   employees:IEmployeeItem[]
@@ -20,10 +24,22 @@ export default class EmployeeDirectory extends React.Component<IEmployeeDirector
   private sp : SPFI;
   private empTableColumns : IColumn[] = [
     {key:"col1",name:"Id",fieldName:"Id",minWidth:50,maxWidth:100,isResizable:true},
+    {key:"col5",name:"Photo",fieldName:"PhotoURL",minWidth:50,maxWidth:100,isResizable:true,onRender:(item,index=0)=>{
+      console.log("Item",item);
+      return (
+        <Image
+        src={"https://picsum.photos/"+`${200+index}`}
+        width={40}
+        height={40}
+        imageFit={ImageFit.cover}
+        alt={item?.Title}
+        styles={{root:{borderRadius:'50%'}}}
+        />
+      )
+    }},
     {key:"col2",name:"Title",fieldName:"Title",minWidth:50,maxWidth:100,isResizable:true,isFiltered:true},
     {key:"col3",name:"Email",fieldName:"Email",minWidth:50,maxWidth:100,isResizable:true},
-    {key:"col4",name:"Department",fieldName:"Department",minWidth:50,maxWidth:100,isResizable:true},
-    //{key:"col5",name:"Photo",fieldName:"PhotoURL",minWidth:50,maxWidth:100,isResizable:true}
+    {key:"col4",name:"Department",fieldName:"Department",minWidth:50,maxWidth:100,isResizable:true}
   ]
   constructor(props:IEmployeeDirectoryProps){
     super(props);
@@ -40,9 +56,9 @@ export default class EmployeeDirectory extends React.Component<IEmployeeDirector
   private getEmployeeDetails =  async (): Promise<IEmployeeItem[]> => {
     //const employees:IEmployeeItem[] = 
     const items = await this.sp.web.lists.getByTitle('Employees').items
-                        //.select("Id","Title","Email","Department","Designation","PhotoURL")
+                        .select("Id","Title","Email","Department","Designation","PhotoURL")
                         .top(100)();
-    console.log("items",items)
+    //console.log("items",items)
     
     return items
   }
